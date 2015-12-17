b58dbf3e7c3ed185c7cb67208b38f0bf65b3728d
tkumata <tkumata@gmail.com>
2015-12-18 08:10:00 +0900

    Initial Commit

---
+++ btCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  btCamera
+//
+//  Created by KUMATA Tomokatsu on 12/18/15.
+//  Copyright © 2015 KUMATA Tomokatsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ btCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  btCamera
+//
+//  Created by KUMATA Tomokatsu on 12/18/15.
+//  Copyright © 2015 KUMATA Tomokatsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
